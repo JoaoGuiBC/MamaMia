51a1cf28fd7c3f35592304556b6a5806d864f75e
joaoguibc <joaoguibc@gmail.com>
2022-04-20 20:55:02 -0300

    Style: Finished sign in page

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { KeyboardAvoidingView, Platform } from 'react-native';
 
+import brandImg from '../../assets/brand.png';
+
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
-import { Container, Content } from './styles';
+import {
+  Container,
+  Content,
+  Title,
+  Brand,
+  ForgotPasswordButton,
+  ForgotPasswordLabel,
+} from './styles';
 
 export function SignIn() {
-  const cucu = () => {
-    console.log('a');
-  };
   return (
     <Container>
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : undefined}
       >
         <Content>
+          <Brand source={brandImg} />
+
+          <Title>Login</Title>
+
           <Input
             placeholder="E-Mail"
             type="secondary"
@@ -25,7 +35,15 @@ export function SignIn() {
           />
           <Input placeholder="Senha" type="secondary" secureTextEntry />
 
-          <Button title="Entrar" type="secondary" onPress={cucu} />
+          <ForgotPasswordButton>
+            <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+          </ForgotPasswordButton>
+
+          <Button
+            title="Entrar"
+            type="secondary"
+            onPress={() => console.log('test')}
+          />
         </Content>
       </KeyboardAvoidingView>
     </Container>
