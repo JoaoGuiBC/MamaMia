132e771240bb2c98162d783e4e8c16841e657535
joaoguibc <joaoguibc@gmail.com>
2022-05-28 00:11:59 -0300

    Feat: Product screen form structure

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
-import { Platform, TouchableOpacity } from 'react-native';
+import { Platform, ScrollView, TouchableOpacity } from 'react-native';
 
-import { BackButton } from '@components/BackButton';
 import { Photo } from '@components/Photo';
+import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { BackButton } from '@components/BackButton';
 import { PriceInput } from '@components/PriceInput';
 
 import {
@@ -14,6 +15,11 @@ import {
   DeleteLabel,
   Upload,
   PickImageButtonContainer,
+  Form,
+  InputGroup,
+  Label,
+  InputGroupHeader,
+  MaxCharacters,
 } from './styles';
 
 export function Product() {
@@ -45,18 +51,45 @@ export function Product() {
           <DeleteLabel>Deletar</DeleteLabel>
         </TouchableOpacity>
       </Header>
+      <ScrollView>
+        <Upload>
+          <Photo uri={image} />
 
-      <Upload>
-        <Photo uri={image} />
+          <PickImageButtonContainer>
+            <Button
+              title="Carregar"
+              type="secondary"
+              onPress={handlePickImage}
+            />
+          </PickImageButtonContainer>
+        </Upload>
 
-        <PickImageButtonContainer>
-          <Button title="Carregar" type="secondary" onPress={handlePickImage} />
-        </PickImageButtonContainer>
-      </Upload>
+        <Form>
+          <InputGroup>
+            <Label>Nome</Label>
+            <Input />
+          </InputGroup>
 
-      <PriceInput size="P" />
-      <PriceInput size="M" />
-      <PriceInput size="G" />
+          <InputGroup>
+            <InputGroupHeader>
+              <Label>Descrição</Label>
+              <MaxCharacters>0 de 60 caracteres</MaxCharacters>
+            </InputGroupHeader>
+
+            <Input multiline maxLength={60} />
+          </InputGroup>
+
+          <InputGroup>
+            <Label>Tamanhos e preços</Label>
+
+            <PriceInput size="P" />
+            <PriceInput size="M" />
+            <PriceInput size="G" />
+          </InputGroup>
+
+          <Button title="Cadastrar pizza" />
+        </Form>
+      </ScrollView>
     </Container>
   );
 }
--- src/screens/Product/styles.ts
+++ src/screens/Product/styles.ts
@@ -42,6 +42,44 @@ export const PickImageButtonContainer = styled(GestureHandlerRootView)`
   margin-left: 32px;
 `;
 
+export const Form = styled.View`
+  width: 100%;
+  padding: 24px;
+`;
+
+export const Label = styled.Text`
+  margin-bottom: 12px;
+  font-size: 14px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TEXT};
+    color: ${theme.COLORS.SECONDARY_900};
+  `}
+`;
+
+export const InputGroup = styled.View`
+  width: 100%;
+  margin-bottom: 16px;
+`;
+
+export const InputGroupHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+`;
+
+export const MaxCharacters = styled.Text`
+  margin-bottom: 10px;
+  font-size: 12px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TEXT};
+    color: ${theme.COLORS.SECONDARY_900};
+  `}
+`;
+
 export const Header = styled(LinearGradient).attrs(({ theme }) => ({
   colors: theme.COLORS.GRADIENT,
 }))`
